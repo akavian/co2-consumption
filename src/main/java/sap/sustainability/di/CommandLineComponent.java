6ae776bfc0d642f9706714f74deb67070974abd4
Ali kavian <akavian@outlook.com>
2025-08-15 00:29:12 +0200

    Adding dependency injection logic.

---
+++ src/main/java/sap/sustainability/di/CommandLineComponent.java
@@ -0,0 +1,18 @@
+package sap.sustainability.di;
+
+import dagger.Component;
+import jakarta.inject.Singleton;
+import picocli.CommandLine;
+
+/** Dagger component for providing dependencies to the command layer. */
+@Singleton
+@Component(modules = {CommandLineProvideModule.class, CommandLineBindModule.class})
+public interface CommandLineComponent {
+
+  /**
+   * Builds and provides an instance of Co2Command.
+   *
+   * @return CommandLine instance
+   */
+  CommandLine getCommandLine();
+}
