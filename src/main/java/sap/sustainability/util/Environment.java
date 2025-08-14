0473a016cf533900b5ef91d6527d6b0183ee2aac
Ali kavian <akavian@outlook.com>
2025-08-15 00:29:41 +0200

    Add environment file to avoid having problem with System.getEnv() in tests.

---
+++ src/main/java/sap/sustainability/util/Environment.java
@@ -0,0 +1,15 @@
+package sap.sustainability.util;
+
+/** Utility class for environment-related operations. */
+public class Environment {
+
+  /**
+   * Returns the value of the specified environment variable.
+   *
+   * @param key the name of the environment variable
+   * @return the value of the environment variable, or {@code null} if not found
+   */
+  public String getEnvironment(String key) {
+    return System.getenv(key);
+  }
+}
