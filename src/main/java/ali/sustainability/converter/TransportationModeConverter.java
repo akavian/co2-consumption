dc01e5f42fb05f4e019333d82f5eef02bb8e50f3
Ali kavian <akavian@outlook.com>
2026-05-02 14:11:58 +0200

    Adding the functionality to read from a properties file instead of hard enum and keep the enum for the fallback.

--- src/main/java/ali/sustainability/converter/TransportationModeConverter.java
+++ src/main/java/ali/sustainability/converter/TransportationModeConverter.java
@@ -3,13 +3,23 @@ package ali.sustainability.converter;
 import picocli.CommandLine.ITypeConverter;
 import ali.sustainability.enums.TransportationMode;
 
+import java.io.IOException;
+import java.util.Properties;
+
 /**
  * Converts transportation mode strings to their corresponding enum values. Used for command-line
  * argument parsing in the CO2 Calculator application.
  */
-public class TransportationModeConverter implements ITypeConverter<TransportationMode> {
+public class TransportationModeConverter implements ITypeConverter<Integer> {
     @Override
-    public TransportationMode convert(String value) {
-        return TransportationMode.fromString(value);
+    public Integer convert(String value) {
+
+        try {
+            Properties properties = new Properties();
+            properties.load(this.getClass().getResourceAsStream("application.properties"));
+            return (int) properties.get(value);
+        } catch (IOException | NullPointerException e) {
+            return TransportationMode.fromString(value).getEmission();
+        }
     }
 }
