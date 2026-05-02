28f454892c26532c95719632a727dd84e9178b35
Ali kavian <akavian@outlook.com>
2026-05-02 14:13:38 +0200

    making the necessary changes for the use of emission rather than the transportation mode.

---
+++ src/main/java/sap/sustainability/service/Co2Calculator.java
@@ -0,0 +1,18 @@
+package sap.sustainability.service;
+
+/**
+ * Interface for calculating CO2 emissions based on transportation mode and city locations.
+ */
+public interface Co2Calculator {
+    /**
+     * Calculates the CO2 emissions for a trip between two cities based on the specified
+     * transportation mode.
+     *
+     * @param startCity the starting city
+     * @param endCity   the destination city
+     * @param emission  the emission of transportation mode used for the trip
+     * @return the calculated CO2 emissions in Kilograms
+     */
+    double calculateEmissions(
+            String startCity, String endCity, int emission);
+}
---
+++ src/main/java/sap/sustainability/service/impl/Co2CalculatorImpl.java
@@ -0,0 +1,58 @@
+package sap.sustainability.service.impl;
+
+import jakarta.inject.Inject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import sap.sustainability.service.Co2Calculator;
+import sap.sustainability.util.Environment;
+import sap.sustainability.util.HttpUtility;
+
+import java.io.IOException;
+
+import static sap.sustainability.ApplicationMessage.*;
+
+/**
+ * Implementation of the CO2 calculator service.
+ */
+public class Co2CalculatorImpl implements Co2Calculator {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Co2CalculatorImpl.class);
+    private final HttpUtility httpUtility;
+    private final Environment environment;
+
+    /**
+     * Constructs a new Co2CalculatorImpl with the specified HTTP utility and environment.
+     *
+     * @param httpUtility the HTTP utility for API calls
+     * @param environment the environment utility for accessing environment variables
+     */
+    @Inject
+    public Co2CalculatorImpl(HttpUtility httpUtility, Environment environment) {
+        this.httpUtility = httpUtility;
+        this.environment = environment;
+    }
+
+    @Override
+    public double calculateEmissions(
+            String startCity, String endCity, int emission) {
+        String apiKey = environment.getEnvironment("ORS_TOKEN");
+        if (apiKey == null || apiKey.isEmpty()) {
+            throw new IllegalStateException(ORS_TOKEN_ENVIRONMENT_VARIABLE_IS_NOT_SET);
+        }
+        try {
+            double[] startCoordinates = httpUtility.fetchCoordinates(startCity, apiKey);
+            double[] endCoordinates = httpUtility.fetchCoordinates(endCity, apiKey);
+            double duration = httpUtility.fetchMatrix(startCoordinates, endCoordinates, apiKey);
+            return (duration * emission) / 1000;
+
+        } catch (IOException e) {
+            LOGGER.error(
+                    FAILED_TO_CALCULATE_EMISSIONS_FROM_TO_USING,
+                    startCity,
+                    endCity,
+                    emission,
+                    e.getMessage());
+            throw new RuntimeException(FAILED_TO_CALCULATE_EMISSIONS_DUE_TO_IO_ERROR, e);
+        }
+    }
+}
